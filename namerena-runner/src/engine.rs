5e292d0058e323f7ae230af79c2a74bdee094c68
shenjack-5600u <3695888@qq.com>
2024-05-02 12:54:52 +0800

    开始进行一个实现

---
+++ namerena-runner/src/engine.rs
@@ -0,0 +1,14 @@
+
+pub mod runners {
+
+    pub struct PlayerGroup {
+        players: Vec<Player>,
+    }
+
+    pub struct Runner {
+        /// 应该是一个 Rc4 实例类似物
+        seed: u32,
+        /// 所有玩家 (包括 boss)
+        players: Vec<PlayerGroup>
+    }
+}
--- namerena-runner/src/main.rs
+++ namerena-runner/src/main.rs
@@ -1,5 +1,7 @@
 /// 万里长征, 始于足下
+
 mod name;
+mod engine;
 mod rc4;
 
 fn main() {
