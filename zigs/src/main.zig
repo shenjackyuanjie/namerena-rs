3e8770c441fb9da4ff4b91825b03911483f0b04a
shenjack <3695888@qq.com>
2024-05-14 20:22:37 +0800

    fine

--- zigs/src/main.zig
+++ zigs/src/main.zig
@@ -1,19 +1,24 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const main_team = Teamer.new("x");
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const count = 20000000;
+    const start_time = std.time.milliTimestamp();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    std.debug.print("start\n", .{});
 
-    try bw.flush(); // don't forget to flush!
+    for (0..count) |i| {
+        // int -> string
+        var buffer: [20]u8 = undefined;
+        const current_name = try std.fmt.bufPrintZ(&buffer, "{d}", .{i});
+        var namer = Namer.new(main_team, current_name);
+        namer.update_skill();
+    }
+
+    const end_time = std.time.milliTimestamp();
+    const elapsed = end_time - start_time;
+    std.debug.print("elapsed: {}\n", .{elapsed});
 }
 
 const val_init = [256]u8{
